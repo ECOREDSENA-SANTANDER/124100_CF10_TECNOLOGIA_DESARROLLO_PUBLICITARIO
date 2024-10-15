a05846dfaa2ca2ecf10d9951e50077d9586871fe
lizmanchego <karinamanchego0416@gmail.com>
2024-10-15 09:58:06 -0500

    FIX Validacion CFA v1.2 - ADD CFA + Material ZIP

--- src/config/global.js
+++ src/config/global.js
@@ -160,7 +160,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF10_124100_DU.pdf',
       },
       {
         icono: 'fas fa-download',
